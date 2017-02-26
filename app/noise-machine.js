8d34d994f8a2613ddc3e00021d8d09b3504594ca
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-02-26 17:59:49 +0000

    initial go at setting something up that makes sounds - programmin in JSON not very good idea, just for lolz

--- app/noise-machine.js
+++ app/noise-machine.js
@@ -1,8 +1,32 @@
 var noiseMachine = {
-    noise : new Howl({
-        src: ['sounds/noise.ogg','sounds/noise.wav']
-    }),
-    playNoise: function(){
-        noise.play();
+    noises : {
+        laser: new Howl({ src: ['sounds/noise.ogg','sounds/noise.wav'] }),
+        drum1: new Howl({ src: ['sounds/drum1.wav'] }),
+        drum2: new Howl({ src: ['sounds/drum2.wav'] })
+    },
+    playNoise: function(noise){
+        var noise =  noiseMachine.noises[noise]
+        if (noise) noise.play();
+    },
+    onKeyDown: function (e) {
+        var letterPressed = String.fromCharCode(e.keyCode)
+        var noiseToPlay = null;
+        switch (letterPressed) {
+            case '0': noiseToPlay = 'laser';
+                break;
+            case '1': noiseToPlay = 'drum1';
+                break;
+            case '2': noiseToPlay = 'drum2';
+                break;
+        }
+        noiseMachine.playNoise(noiseToPlay);
+        if( e.preventDefault) e.preventDefault();
+        return false;
+    },
+    init : function(){
+        document.getElementById('drum-machine').onkeydown = this.onKeyDown
     }
 }
+
+noiseMachine.init();
+
