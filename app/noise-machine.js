fe0b75b8cd6d0cf5373589f45611766b64a8f3b1
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-02-27 13:19:56 +0000

    easier mapping of noises to keys, using keypress instead of keydown

--- app/noise-machine.js
+++ app/noise-machine.js
@@ -3,57 +3,33 @@ function loadSfx(soundName){
 }
 var noiseMachine = {
     noises : {
-        laser: this.loadSfx('laser'),
-        drum1: this.loadSfx('drum1'),
-        drum2: this.loadSfx('drum2'),
-        drum2: this.loadSfx('drum2'),
-        ding: this.loadSfx('ding'),
-        dong: this.loadSfx('dong'),
-        doorbell: this.loadSfx('doorbell'),
-        mobile: this.loadSfx('mobile'),
-        scratch: this.loadSfx('scratch'),
-        shotgun1: this.loadSfx('shotgun-reload'),
-        shotgun2: this.loadSfx('shotgun-fire'),
-        loop:  this.loadSfx('loop')
+        laser: loadSfx('laser'),
+        drum1: loadSfx('drum1'),
+        drum2: loadSfx('drum2'),
+        drum2: loadSfx('drum2'),
+        ding: loadSfx('ding'),
+        dong: loadSfx('dong'),
+        doorbell: loadSfx('doorbell'),
+        mobile: loadSfx('mobile'),
+        scratch: loadSfx('scratch'),
+        shotgun1: loadSfx('shotgun-reload'),
+        shotgun2: loadSfx('shotgun-fire'),
+        loop: loadSfx('loop')
     },
+    noiseMappings : { '0' : 'laser', '1' : 'drum1', '2' : 'drum2', '3' : 'loop', '4' : 'ding', '5' : 'dong', '6' : 'doorbell', '7' : 'mobile', '8' : 'scratch', 'q' : 'shotgun1', 'w' : 'shotgun2' },
     playNoise: function(noise){
         var noise =  noiseMachine.noises[noise]
         if (noise) noise.play();
     },
-    keyDownHandler: function (e) {
+    keyHandler: function (e) {
         var letterPressed = String.fromCharCode(e.keyCode)
-        var noiseToPlay = null;
-        switch (letterPressed) {
-            case '0': noiseToPlay = 'laser';
-                break;
-            case '1': noiseToPlay = 'drum1';
-                break;
-            case '2': noiseToPlay = 'drum2';
-                break;
-            case '3': noiseToPlay = 'loop';
-                break;
-            case '4': noiseToPlay = 'ding';
-                break;
-            case '5': noiseToPlay = 'dong';
-                break;
-            case '6': noiseToPlay = 'doorbell';
-                break;
-            case '7': noiseToPlay = 'mobile';
-                break;
-            case '8': noiseToPlay = 'scratch';
-                break;
-            case 'Q': noiseToPlay = 'shotgun1';
-                break;
-            case 'W': noiseToPlay = 'shotgun2';
-                break;
-        }
-        noiseMachine.playNoise(noiseToPlay);
+        noiseMachine.playNoise(noiseMachine.noiseMappings[letterPressed]);
         if( e.preventDefault) e.preventDefault();
         return false;
     },
     init : function(id){
         var input = document.getElementById(id);
-        input.onkeydown = this.keyDownHandler;
+        input.onkeypress = this.keyHandler;
         input.focus();
     }
 }
